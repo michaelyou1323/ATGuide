efff1a3fa338783eacd376053ebe3dc52bfc406f
michaelyou1323 <73140783+michaelyou1323@users.noreply.github.com>
2023-10-07 09:40:58 +0300

    Initial Commit

---
+++ ATGuide/ATGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATGuideApp.swift
+//  ATGuide
+//
+//  Created by Michaelyoussef on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ATGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
