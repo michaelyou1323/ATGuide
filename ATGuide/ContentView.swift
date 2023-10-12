8713096f053e14b57d05686bc78fedcf19a161dd
michaelyou1323 <73140783+michaelyou1323@users.noreply.github.com>
2023-10-12 15:15:51 +0300

    commit nm

--- ATGuide/ContentView.swift
+++ ATGuide/ContentView.swift
@@ -20,18 +20,21 @@ struct ContentView: View {
                     
                     Button(action: {
                         // Perform login action
+                        
                         isShowingNewView = true
                         login()
                     }) {
-                        Text("Login")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(10)
+                        NavigationLink(destination: MainScreen()) {
+                            Text("Login")
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        
+                        .padding()
                     }
-                    .padding()
-                    
                     Button(action: {
                         // Perform signup action
                         signUp()
@@ -45,26 +48,16 @@ struct ContentView: View {
                     }
                     .padding()
                 }
-                .navigationBarTitleDisplayMode(.automatic)
-                .navigationBarItems(leading: Button(action: {
-                    // Toggle side menu visibility
-                    withAnimation(.easeInOut) {
-                        presentSideMenu.toggle()
-                    }
-                }) {
-                    Image(systemName: "line.horizontal.3")
-                        .imageScale(.large)
-                })
+              
             }
-            if presentSideMenu {
-                SideMenu(presentSideMenu: $presentSideMenu)
-            }
+            
         }
         .background(Color.black.opacity(0.05))
     }
     
     func login() {
         // Perform login functionality
+        
     }
     
     func signUp() {
@@ -72,46 +65,6 @@ struct ContentView: View {
     }
 }
 
-struct SideMenu: View {
-    @Binding var presentSideMenu: Bool
-    
-    var body: some View {
-        ZStack(alignment: .leading ) {
-            Color.gray.opacity(0.4).edgesIgnoringSafeArea(.all).onTapGesture { withAnimation { presentSideMenu = false } }
-            
-            VStack(alignment: .leading) {
-                HStack{
-                    Image(systemName: "person.crop.circle")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                        .clipShape(Circle())
-                    VStack(alignment:.leading){
-                        Text("User Name").font(.headline)
-                        Text("language").font(.subheadline)
-                    }.padding(.leading, 1)
-                }.padding(.top, 1).padding(.leading, 10).padding(.bottom, 120)
-                 
-                Text("Setting").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    .padding(.leading, 8)
-
-                Text("New Broadcast").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    .padding(.leading, 8)
-                
-                Text("WhatsApp Web").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    .padding(.leading, 8)
-                
-                Text("Starred Messages").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    .padding(.leading, 8)
-                
-                Text("Settings").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    .padding(.leading, 8)
-                
-            }.frame(maxWidth:200, maxHeight:.infinity ,alignment:.topLeading).background(Color.white.ignoresSafeArea(.all)).offset(x:self.presentSideMenu ? 0 : -UIScreen.main.bounds.width/4)
-                
-                
-        }
-    }
-}
 
 struct ContentView_Previews1 : PreviewProvider {
     static var previews : some View{
--- ATGuide/Preview Content/MainScreen.swift
+++ ATGuide/Preview Content/MainScreen.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct MainScreen: View {
+    @State private var isShowingNewView = false
+    @State private var presentSideMenu = false
     var body: some View {
-        
+        ZStack {
         NavigationView {
-            
             VStack {
                 Text("ATGuide").font(.system(size: 33))
                     .bold()
-                    .padding(2)
+                   
                 
                 
                 TabView {
@@ -53,17 +54,74 @@ struct MainScreen: View {
                     
                     
                 }
+                
             }
             
-            // .foregroundColor(Color.red)
-            
-//            NavigationLink(destination: FormScreen()) {
-//                
-//            }
-            
-            
+            .navigationBarTitleDisplayMode(.automatic)
+            .navigationBarItems(leading: Button(action: {
+                // Toggle side menu visibility
+                withAnimation(.easeInOut) {
+                    presentSideMenu.toggle()
+                }
+            }) {
+                Image(systemName: "line.horizontal.3")
+                    .imageScale(.large)
+            })
         }
+        
         .navigationBarHidden(true)
+        if presentSideMenu {
+            SideMenu(presentSideMenu: $presentSideMenu)
+        }
+    }
+        
+    .background(Color.black.opacity(0.05))
+
+        }
+        
+       
+    }
+
+
+struct SideMenu: View {
+    @Binding var presentSideMenu: Bool
+    
+    var body: some View {
+        ZStack(alignment: .leading ) {
+            Color.gray.opacity(0.4).edgesIgnoringSafeArea(.all).onTapGesture { withAnimation { presentSideMenu = false } }
+            
+            VStack(alignment: .leading) {
+                HStack{
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(Circle())
+                    VStack(alignment:.leading){
+                        Text("User Name").font(.headline)
+                        Text("language").font(.subheadline)
+                    }.padding(.leading, 1)
+                }.padding(.top, 1).padding(.leading, 10).padding(.bottom, 120)
+                 
+                Text("Setting").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    .padding(.leading, 8)
+
+                Text("Planning").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    .padding(.leading, 8)
+                
+                
+                Text("about").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    .padding(.leading, 8)
+                
+                Text("Privacy").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                    .padding(.leading, 8)
+                
+                Text("Contact Us").font(.headline).padding().background(Color.white).cornerRadius(10).shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                                    .padding(.leading, 8)
+                                
+            }.frame(maxWidth:195, maxHeight:.infinity ,alignment:.topLeading).background(Color.white.ignoresSafeArea(.all)).offset(x:self.presentSideMenu ? 0 : -UIScreen.main.bounds.width/4)
+                
+                
+        }
     }
 }
 
